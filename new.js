826a9ce837fac6fd8d51eeda74ae8cdedabd03f0
krushnabhamare <152904939+krushnabhamare@users.noreply.github.com>
2024-05-20 21:57:01 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,224 @@
+// music for start and end 
+start = new Audio('start.mp3');
+setTimeout(() => {
+    start.play()
+},6000);
+setTimeout(() => {
+    start.pause()
+},23000);
+
+over = new Audio('over.mp3');
+setTimeout(() => {
+    over.play()
+},23000);
+
+
+// program 
+const touchme = () => { 
+     //for dog
+    dog1 = document.querySelector(".dog1");
+    dog1.classList.add('dog');
+    setTimeout(() => {
+        dog1.classList.remove('dog');
+    }, 1500);
+    //for bird
+    d1 = document.querySelector(".d1");
+    d1.classList.add('kill');
+    UScore++;
+    mysc++;
+    UserScore.innerText = UScore ;
+    myScore.innerText = mysc;
+    //gun shoot sound
+    let mySound = new Audio('audio.mp3');
+    setTimeout(() => {
+    mySound.play()
+    },0);
+}/*krushna bhamare*/
+const touchme1 = () => {
+    //for dog
+    dog1 = document.querySelector(".dog1");
+    dog1.classList.add('dog');
+    setTimeout(() => {
+        dog1.classList.remove('dog');
+    }, 1500);
+    //for bird
+    d2 = document.querySelector(".d2");
+    d2.classList.add('kill1');
+    UScore++;
+    mysc++;
+    UserScore.innerText = UScore ;
+    myScore.innerText = mysc;
+    //gun shoot sound
+    let mySound = new Audio('audio.mp3');
+    setTimeout(() => {
+    mySound.play()
+    },0);
+}
+const touchme2 = () => {
+     //for dog
+    dog1 = document.querySelector(".dog1");
+    dog1.classList.add('dog');
+    setTimeout(() => {
+        dog1.classList.remove('dog');
+    }, 1500);
+    //for bird
+    d3 = document.querySelector(".d3");
+    d3.classList.add('kill2');
+    UScore++;
+    mysc++;
+    UserScore.innerText = UScore ;
+    myScore.innerText = mysc;
+    //gun shoot sound
+    let mySound = new Audio('audio.mp3');
+    setTimeout(() => {
+    mySound.play()
+    },0);
+}
+const touchme3 = () => {
+     //for dog
+    dog1 = document.querySelector(".dog1");
+    dog1.classList.add('dog');
+    setTimeout(() => {
+        dog1.classList.remove('dog');
+    }, 1500);
+    //for bird
+    d4 = document.querySelector(".d4");
+    d4.classList.add('kill3');
+    UScore++;
+    mysc++;
+    UserScore.innerText = UScore ;
+    myScore.innerText = mysc;
+    //gun shoot sound
+    let mySound = new Audio('audio.mp3');
+    setTimeout(() => {
+    mySound.play()
+    },0);
+}
+const touchme4 = () => {
+     //for dog
+    dog1 = document.querySelector(".dog1");
+    dog1.classList.add('dog');
+    setTimeout(() => {
+        dog1.classList.remove('dog');
+    }, 1500);
+    //for bird
+    d5 = document.querySelector(".d5");
+    d5.classList.add('kill4');
+    UScore++;
+    mysc++;
+    UserScore.innerText = UScore ;
+    myScore.innerText = mysc;
+    //gun shoot sound
+    let mySound = new Audio('audio.mp3');
+    setTimeout(() => {
+    mySound.play()
+    },0);
+}/*krushna bhamare*/
+const touchme5 = () => {
+     //for dog
+    dog1 = document.querySelector(".dog1");
+    dog1.classList.add('dog');
+    setTimeout(() => {
+        dog1.classList.remove('dog');
+    }, 1500);
+    //for bird
+    d6 = document.querySelector(".d6");
+    d6.classList.add('kill5');
+    UScore++;
+    mysc++;
+    UserScore.innerText = UScore ;
+    myScore.innerText = mysc;
+    //gun shoot sound
+    let mySound = new Audio('audio.mp3');
+    setTimeout(() => {
+    mySound.play()
+    },0);
+}
+const touchme6 = () => {
+     //for dog
+    dog1 = document.querySelector(".dog1");
+    dog1.classList.add('dog');
+    setTimeout(() => {
+        dog1.classList.remove('dog');
+    }, 1500);
+    //for bird
+    d7 = document.querySelector(".d7");
+    d7.classList.add('kill6');
+    UScore++;
+    mysc++;
+    UserScore.innerText = UScore ;
+    myScore.innerText = mysc;
+    //gun shoot sound
+    let mySound = new Audio('audio.mp3');
+    setTimeout(() => {
+    mySound.play()
+    },0);
+}
+const touchme7 = () => {
+     //for dog
+    dog1 = document.querySelector(".dog1");
+    dog1.classList.add('dog');
+    setTimeout(() => {
+        dog1.classList.remove('dog');
+    }, 1500);
+    //for bird
+    d8 = document.querySelector(".d8");
+    d8.classList.add('kill7');
+    UScore++;
+    mysc++;
+    UserScore.innerText = UScore ;
+    myScore.innerText = mysc;
+    //gun shoot sound
+    let mySound = new Audio('audio.mp3');
+    setTimeout(() => {
+    mySound.play()
+    },0);
+}/*krushna bhamare*/
+const touchme8 = () => {
+     //for dog
+    dog1 = document.querySelector(".dog1");
+    dog1.classList.add('dog');
+    setTimeout(() => {
+        dog1.classList.remove('dog');
+    }, 1500);
+    //for bird
+    d9 = document.querySelector(".d9");
+    d9.classList.add('kill8');
+    UScore++;
+    mysc++;
+    UserScore.innerText = UScore ;
+    myScore.innerText = mysc;
+    //gun shoot sound
+    let mySound = new Audio('audio.mp3');
+    setTimeout(() => {
+    mySound.play()
+    },0);
+}
+const touchme9 = () => {
+     //for dog
+    dog1 = document.querySelector(".dog1");
+    dog1.classList.add('dog');
+    setTimeout(() => {
+        dog1.classList.remove('dog');
+    }, 1500);
+    //for bird
+    d10 = document.querySelector(".d10");
+    d10.classList.add('kill9');
+    UScore++;
+    mysc++;
+    UserScore.innerText = UScore ;
+    myScore.innerText = mysc;
+    //gun shoot sound
+    let mySound = new Audio('audio.mp3');
+    setTimeout(() => {
+    mySound.play()
+    },0);
+}
+
+const UserScore = document.querySelector("#usersc");
+const myScore = document.querySelector(".endsc");
+let UScore = 0 ;
+let mysc = 0 ;
+
+                                                                            //music
+  
